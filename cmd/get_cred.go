5766ae3b25c4e0c586ef56749cd51bf6590b79a8
Ilya Verbitskiy <ilya@verbit.io>
2020-02-13 12:56:25 +0100

    Rewrite core parts of the OIDC flow.
    This new rewrite is now based on the (almost) standard OAuth2 Go library.

--- cmd/get_cred.go
+++ cmd/get_cred.go
@@ -2,17 +2,19 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"net/http"
 	"strconv"
-	"strings"
 	"time"
 
-	pkce "github.com/nirasan/go-oauth-pkce-code-verifier"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+
 	"github.com/pkg/browser"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
-	"golang.org/x/net/context"
 )
 
 var getCredCmd = &cobra.Command{
@@ -20,17 +22,6 @@ var getCredCmd = &cobra.Command{
 	Short: "Get AWS credentials and out to stdout",
 	Long:  `Get AWS credentials and out to stdout through your OIDC provider authentication.`,
 	Run:   getCred,
-}
-
-type TokenResponse struct {
-	AccessToken      string `json:"access_token"`
-	IDToken          string `json:"id_token"`
-	RefreshToken     string `json:"refresh_token"`
-	ExpiresIn        int64  `json:"expires_in"`
-	RefreshExpiresIn int64  `json:"refresh_expires_in"`
-}
-
-type loginFlagsStruct struct {
 }
 
 type AWSCredentials struct {
@@ -79,8 +70,9 @@ func getCred(cmd *cobra.Command, args []string) {
 		Exit(err)
 	}
 
+	idToken := tokenResponse.Extra("id_token").(string)
 	Writeln("Login successful!")
-	Traceln("ID token: %s", tokenResponse.IDToken)
+	Traceln("ID token: %s", idToken)
 
 	awsFedType := client.config.GetString(AWS_FEDERATION_TYPE)
 	maxSessionDurationSecondsString := client.config.GetString(MAX_SESSION_DURATION_SECONDS)
@@ -91,7 +83,7 @@ func getCred(cmd *cobra.Command, args []string) {
 
 	var awsCreds *AWSCredentials
 	if awsFedType == AWS_FEDERATION_TYPE_OIDC {
-		awsCreds, err = GetCredentialsWithOIDC(client, tokenResponse.IDToken, maxSessionDurationSeconds)
+		awsCreds, err = GetCredentialsWithOIDC(client, idToken, maxSessionDurationSeconds)
 		if err != nil {
 			Writeln("Failed to get aws credentials with OIDC")
 			Exit(err)
@@ -108,52 +100,34 @@ func getCred(cmd *cobra.Command, args []string) {
 	Export("AWS_SESSION_TOKEN", awsCreds.AWSSessionToken)
 }
 
-func doLogin(client *OIDCClient) (*TokenResponse, error) {
+func doLogin(client *OIDCClient) (*oauth2.Token, error) {
 	listener, err := net.Listen("tcp", "127.0.0.1:")
 	if err != nil {
 		return nil, errors.Wrap(err, "Cannot start local http server to handle login redirect")
 	}
 	port := listener.Addr().(*net.TCPAddr).Port
 
-	clientId := client.config.GetString(CLIENT_ID)
 	redirect := fmt.Sprintf("http://127.0.0.1:%d", port)
-	v, err := pkce.CreateCodeVerifierWithLength(pkce.MaxLength)
+
+	ctx := context.Background()
+	conf := &oauth2.Config{
+		ClientID:     client.config.GetString(CLIENT_ID),
+		ClientSecret: client.config.GetString(CLIENT_SECRET),
+		Endpoint:     google.Endpoint,
+		RedirectURL:  redirect,
+		Scopes:       []string{"openid", "email"},
+	}
+
+	url := conf.AuthCodeURL("state")
+
+	code := launch(client, url, listener)
+
+	tok, err := conf.Exchange(ctx, code)
 	if err != nil {
-		return nil, errors.Wrap(err, "Cannot generate OAuth2 PKCE code_challenge")
+		log.Fatal(err)
 	}
-	challenge := v.CodeChallengeS256()
-	verifier := v.String()
 
-	authReq := client.Authorization().
-		QueryParam("response_type", "code").
-		QueryParam("client_id", clientId).
-		QueryParam("redirect_uri", redirect).
-		QueryParam("code_challenge", challenge).
-		QueryParam("code_challenge_method", "S256").
-		QueryParam("scope", "openid")
-
-	additionalQuery := client.config.GetString(OIDC_AUTHENTICATION_REQUEST_ADDITIONAL_QUERY)
-	if additionalQuery != "" {
-		queries := strings.Split(additionalQuery, "&")
-		for _, q := range queries {
-			kv := strings.Split(q, "=")
-			if len(kv) == 1 {
-				authReq = authReq.QueryParam(kv[0], "")
-			} else if len(kv) == 2 {
-				authReq = authReq.QueryParam(kv[0], kv[1])
-			} else {
-				return nil, errors.Errorf("Invalid additional query: %s", q)
-			}
-		}
-	}
-	url := authReq.Url()
-
-	code := launch(client, url.String(), listener)
-	if code != "" {
-		return codeToToken(client, verifier, code, redirect)
-	} else {
-		return nil, errors.New("Login failed, can't retrieve authorization code")
-	}
+	return tok, err
 }
 
 func launch(client *OIDCClient, url string, listener net.Listener) string {
@@ -222,50 +196,4 @@ func launch(client *OIDCClient, url string, listener net.Listener) string {
 	}
 
 	return code
-}
-
-func GetFreePort() (int, error) {
-	addr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:0")
-	if err != nil {
-		return 0, err
-	}
-
-	l, err := net.ListenTCP("tcp", addr)
-	if err != nil {
-		return 0, err
-	}
-	defer l.Close()
-	return l.Addr().(*net.TCPAddr).Port, nil
-}
-
-func codeToToken(client *OIDCClient, verifier string, code string, redirect string) (*TokenResponse, error) {
-	form := client.ClientForm()
-	form.Set("grant_type", "authorization_code")
-	form.Set("code", code)
-	form.Set("code_verifier", verifier)
-	form.Set("redirect_uri", redirect)
-
-	Traceln("code2token params:", form)
-
-	res, err := client.Token().Request().Form(form).Post()
-
-	if err != nil {
-		return nil, errors.Wrap(err, "Failed to turn code into token")
-	}
-
-	if res.Status() != 200 {
-		if res.MediaType() != "" {
-			var json map[string]interface{}
-			err := res.ReadJson(&json)
-			if err == nil {
-				return nil, errors.Errorf("Failed to turn code into token, error: %s error_description: %s",
-					json["error"], json["error_description"])
-			}
-		}
-		return nil, errors.Errorf("Failed to turn code into token")
-	}
-
-	var tokenResponse TokenResponse
-	res.ReadJson(&tokenResponse)
-	return &tokenResponse, nil
 }
