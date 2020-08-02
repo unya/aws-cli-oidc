9495e9ba3be7414c8f64a68da488a879bb83a03d
Ilya Verbitskiy <ilya@verbit.io>
2020-08-02 16:49:06 +0200

    sign-in: make it work with all kinds of profiles

--- cmd/aws-sign-in/main.go
+++ cmd/aws-sign-in/main.go
@@ -1,21 +1,18 @@
 package main
 
 import (
-	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
-	"os"
-	"os/exec"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/docopt/docopt-go"
-	"github.com/mattn/go-shellwords"
-	"github.com/mbrtargeting/aws-cli-oidc/internal"
 	"github.com/pkg/browser"
-	"gopkg.in/ini.v1"
 )
 
 func main() {
@@ -24,7 +21,7 @@ func main() {
 	usage := `aws-sign-in.
 
 Usage:
-  aws-sign-in [<profile>]
+  aws-sign-in
   aws-sign-in -h | --help
 
 Options:
@@ -36,25 +33,20 @@ Options:
 	}
 
 	var conf struct {
-		Profile string `docopt:"<profile>"`
 	}
 	if err := arguments.Bind(&conf); err != nil {
 		log.Fatalf("%v\n", err)
 	}
 
-	profileName := conf.Profile
-	if profileName == "" {
-		profileName = os.Getenv("AWS_PROFILE")
-		if profileName == "" {
-			profileName = "default"
-		}
-	}
-
-	configPath, credentialsPath := getAWSConfigPaths()
-
-	creds, err := runCredentialProcess(configPath, credentialsPath, profileName)
+	cfg, err := external.LoadDefaultAWSConfig()
 	if err != nil {
-		log.Fatalf("Error running the credential process: %v\n", err)
+		fmt.Printf("unable to load SDK config: %v\n", err)
+	}
+	cfg.Region = "eu-central-1"
+
+	creds, err := cfg.Credentials.Retrieve(context.Background())
+	if err != nil {
+		log.Fatalf("Error retrieving credentials: %v\n", err)
 	}
 
 	signinToken, err := getSignInToken(creds)
@@ -70,94 +62,7 @@ Options:
 	fmt.Printf("If the browser didn't open, please visit the following url to sign in to the AWS console: %v\n", loginURL)
 }
 
-func getAWSConfigPaths() (string, string) {
-	var credentialsPath string
-	var configPath string
-
-	awsConfigFile := os.Getenv("AWS_CONFIG_FILE")
-	if awsConfigFile != "" {
-		configPath = awsConfigFile
-	}
-
-	awsCredentialsFile := os.Getenv("AWS_SHARED_CREDENTIALS_FILE")
-	if awsCredentialsFile != "" {
-		credentialsPath = awsCredentialsFile
-	}
-
-	home, err := os.UserHomeDir()
-	if err == nil {
-		// only overwrite the paths if the respective env var was not set
-		if configPath == "" {
-			configPath = home + "/.aws/config"
-		}
-		if credentialsPath == "" {
-			credentialsPath = home + "/.aws/credentials"
-		}
-	}
-
-	return configPath, credentialsPath
-}
-
-func tryRunCredentialProcess(credentialProcess string) (*internal.AWSCredentialsJSON, error) {
-	words, err := shellwords.Parse(credentialProcess)
-	if err != nil || len(words) == 0 {
-		return nil, fmt.Errorf("invalid credential_process entry")
-	}
-	cmd := exec.Command(words[0], words[1:]...)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	if err := cmd.Run(); err != nil {
-		return nil, fmt.Errorf("failed to run the credential process: %v", err)
-	}
-
-	var creds internal.AWSCredentialsJSON
-	err = json.Unmarshal(out.Bytes(), &creds)
-	if err != nil {
-		return nil, fmt.Errorf("error parsing credential process output: %v", err)
-	}
-
-	return &creds, nil
-}
-
-func findCredentialProcess(path string, profile string) (string, error) {
-	cfg, err := ini.Load(path)
-	if err != nil {
-		return "", fmt.Errorf("failed to read file: %v", err)
-	}
-
-	section, err := cfg.GetSection(profile)
-	if err != nil {
-		return "", fmt.Errorf("failed to read section: %v", err)
-	}
-
-	key, err := section.GetKey("credential_process")
-	if err != nil {
-		return "", fmt.Errorf("failed to find credential_process entry: %v", err)
-	}
-
-	return key.String(), nil
-}
-
-func runCredentialProcess(configPath string, credentialsPath string, profile string) (*internal.AWSCredentialsJSON, error) {
-	credentialProcess, err := findCredentialProcess(credentialsPath, profile)
-	if err == nil {
-		return tryRunCredentialProcess(credentialProcess)
-	}
-
-	// yes, the ~/.aws/config has a different naming scheme for profile names (must be prefixed with "profile")
-	configProfileName := profile
-	if profile != "default" {
-		configProfileName = fmt.Sprintf("profile %s", profile)
-	}
-	credentialProcess, err = findCredentialProcess(configPath, configProfileName)
-	if err == nil {
-		return tryRunCredentialProcess(credentialProcess)
-	}
-
-	return nil, fmt.Errorf("not able to find a valid credential_process")
-}
-
-func getSignInToken(creds *internal.AWSCredentialsJSON) (string, error) {
+func getSignInToken(creds aws.Credentials) (string, error) {
 	reqJSON := fmt.Sprintf(`{"sessionId":"%s","sessionKey":"%s","sessionToken":"%s"}`,
 		creds.AccessKeyID,
 		creds.SecretAccessKey,
--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/mbrtargeting/aws-cli-oidc
 
 require (
 	github.com/aws/aws-sdk-go v1.15.50
+	github.com/aws/aws-sdk-go-v2 v0.24.0
 	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
 	github.com/mattn/go-shellwords v1.0.10
 	github.com/natsukagami/go-input v0.0.0-20180603034138-38bb793e9754
@@ -10,9 +11,9 @@ require (
 	github.com/stretchr/testify v1.5.1 // indirect
 	github.com/zalando/go-keyring v0.0.0-20200121091418-667557018717
 	golang.org/x/crypto v0.0.0-20200210222208-86ce3cb69678 // indirect
-	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
-	gopkg.in/ini.v1 v1.52.0
+	gopkg.in/ini.v1 v1.52.0 // indirect
 	gopkg.in/yaml.v2 v2.2.2
 )
 
